4bac107b799b42bada9c9cfc527b5df1c008f3c4
Olivier Aubert <contact@olivieraubert.net>
2013-05-21 17:39:15 +0200

    Display tags in admin changelist

--- kadist/admin.py
+++ kadist/admin.py
@@ -2,17 +2,24 @@ from django.contrib import admin
 from .models import Artist, Work
 
 class WorkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'worktype', 'year', 'creator', 'technique', 'description', 'tags')
+    fields = ('title', 'tags', 'worktype', 'year', 'creator', 'technique', 'url', 'description')
+    list_display = ('title', 'tags_as_string', 'worktype', 'year', 'creator', 'technique', 'description')
     list_display_links = ('title',)
     list_filter = ( 'creator', 'worktype', 'year' )
     search_fields = [ 'creator', 'title', 'description' ]
+    def tags_as_string(self, obj):
+        return ", ".join(t.name for t in obj.tags.all())
+    tags_as_string.short_description = 'Tags'
 admin.site.register(Work, WorkAdmin)
 
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ('name', 'country', 'description', 'tags' )
+    fields = ('name', 'tags', 'country', 'url', 'description' )
+    list_display = ('name', 'tags_as_string', 'country', 'description' )
     list_display_links = ('name',)
     list_filter = ( 'country', )
     search_fields = [ 'name', 'country', 'description' ]
-
+    def tags_as_string(self, obj):
+        return ", ".join(t.name for t in obj.tags.all())
+    tags_as_string.short_description = 'Tags'
 admin.site.register(Artist, ArtistAdmin)
 
