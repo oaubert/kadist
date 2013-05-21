b9ab849474c36826c1372eb943a1273b009d7fdf
Olivier Aubert <contact@olivieraubert.net>
2013-05-21 22:15:30 +0200

    Define favicon and redirect root to kadist/

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,12 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+                       url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': settings.STATIC_URL + 'img/favicon.ico'}),
+                       url(r'^$', RedirectView.as_view(url='/kadist/')),
                        url(r'^kadist/', include('kadist.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^grappelli/', include('grappelli.urls')),
