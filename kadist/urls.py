0bdebf951bd931e6f3a4c8c25b1ab3a5af0d9492
Olivier Aubert <contact@olivieraubert.net>
2013-05-21 22:16:01 +0200

    Implement tag listing and display

--- kadist/urls.py
+++ kadist/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import patterns, url, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.views.generic import RedirectView
 from django.contrib import admin
 from kadist import views
 
 urlpatterns = patterns('',
-                       url(r'^work/$', views.WorkList.as_view(), name='work-list'),
-                       url(r'^work/(?P<pk>[0-9]+)/$', views.WorkDetail.as_view(), name='work-detail'), 
-                       url(r'^artist/$', views.ArtistList.as_view(), name='artist-list'),
-                       url(r'^artist/(?P<pk>[0-9]+)/$', views.ArtistDetail.as_view(), name='artist-detail'), 
+                       url(r'^$', RedirectView.as_view(url='tag/', permanent=True)),
+                       url(r'^tag/$', views.taglist, name='tag-list'),
+                       url(r'^tag/(?P<kw>.+)$', views.tag, name='tag-detail'),
+                       url(r'^api/work/$', views.WorkList.as_view(), name='api-work-list'),
+                       url(r'^api/work/(?P<pk>[0-9]+)/$', views.WorkDetail.as_view(), name='api-work-detail'), 
+                       url(r'^api/artist/$', views.ArtistList.as_view(), name='api-artist-list'),
+                       url(r'^api/artist/(?P<pk>[0-9]+)/$', views.ArtistDetail.as_view(), name='api-artist-detail'), 
                        )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
@@ -15,6 +19,5 @@ urlpatterns = format_suffix_patterns(urlpatterns)
 urlpatterns += patterns('',
                         url(r'^api-auth/', include('rest_framework.urls',
                                                    namespace='rest_framework')),
-                        url(r'^$', views.api_root),
                         )
 
--- kadist/views.py
+++ kadist/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.conf import settings
+from django.db.models.loading import get_model
+
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -6,11 +12,33 @@ from rest_framework.reverse import reverse
 from .models import Artist, Work
 from .serializers import ArtistSerializer, WorkSerializer
 
+# define the default models for tags and tagged items
+TAG_MODEL = get_model('taggit', 'Tag')
+
 @api_view(('GET',))
 def api_root(request, format=None):
     return Response({
             'works': reverse('work-list', request=request),
             })
+
+@login_required
+def taglist(request):
+    keywords = TAG_MODEL.objects.all().values_list('name', flat=True)
+    return render_to_response('main.html', {
+            'keywords': keywords
+            }, context_instance=RequestContext(request))
+
+@login_required
+def tag(request, kw=None):
+    works = Work.objects.filter(tags__name__in=[kw])
+    artists = Artist.objects.filter(tags__name__in=[kw])
+    synonyms = []
+    return render_to_response('tag.html', {
+            'kw': kw,
+            'works': works,
+            'artists': artists,
+            'synonyms': synonyms,
+            }, context_instance=RequestContext(request))
 
 class WorkList(generics.ListCreateAPIView):
     model = Work
