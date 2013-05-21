ccb2865ebb1db6e678c8820f69d174c8e144dfab
Olivier Aubert <contact@olivieraubert.net>
2013-05-21 16:44:50 +0200

    Serialize tags

--- kadist/serializers.py
+++ kadist/serializers.py
@@ -3,14 +3,18 @@ from .models import Work, Artist
 
 class WorkSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(source='pk', read_only=True)
+    tags = serializers.ManyRelatedField(source='tags')
 
     class Meta:
         model = Work
-        fields = ('id', 'creator', 'title', 'worktype', 'technique','dimensions', 'description')
+        fields = ('id', 'creator', 'title', 'worktype', 'technique','dimensions', 'description', 'tags')
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(source='pk', read_only=True)
+    works = serializers.HyperlinkedRelatedField(many=True, read_only=True,
+                                                view_name='work-detail')
+    tags = serializers.ManyRelatedField(source='tags')
 
     class Meta:
         model = Artist
-        fields = ('name', 'country', 'description')
+        fields = ('name', 'country', 'description', 'works', 'tags')
