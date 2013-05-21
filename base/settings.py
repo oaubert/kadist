3e6ae234a738e75498ddc1982d918220f1ec3e9b
Olivier Aubert <contact@olivieraubert.net>
2013-05-21 16:14:54 +0200

    Make static/media dir relative to settings.py path

--- base/settings.py
+++ base/settings.py
@@ -1,7 +1,11 @@
 # Django settings for base project.
 
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+import os
+APPROOT = os.path.dirname(os.path.dirname(__file__)) + os.sep
 
 ADMINS = (
     ('Olivier Aubert', 'contact@olivieraubert.net'),
@@ -12,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': APPROOT + 'db.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -45,7 +49,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(APPROOT, "media/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -56,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/oaubert/src/kadist/base/static'
+STATIC_ROOT = APPROOT + 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
