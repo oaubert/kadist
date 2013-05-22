8525ee14fb936bc3654b75cc83d546c81feb1e0e
Olivier Aubert <contact@olivieraubert.net>
2013-05-22 16:27:48 +0200

    Add root page

--- kadist/urls.py
+++ kadist/urls.py
@@ -5,13 +5,13 @@ from django.contrib import admin
 from kadist import views
 
 urlpatterns = patterns('',
-                       url(r'^$', RedirectView.as_view(url='tag/', permanent=True)),
+                       url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'root.html'}),
                        url(r'^tag/$', views.taglist, name='tag-list'),
                        url(r'^tag/(?P<kw>.+)$', views.tag, name='tag-detail'),
                        url(r'^api/work/$', views.WorkList.as_view(), name='api-work-list'),
-                       url(r'^api/work/(?P<pk>[0-9]+)/$', views.WorkDetail.as_view(), name='api-work-detail'), 
+                       url(r'^api/work/(?P<pk>[0-9]+)/$', views.WorkDetail.as_view(), name='api-work-detail'),
                        url(r'^api/artist/$', views.ArtistList.as_view(), name='api-artist-list'),
-                       url(r'^api/artist/(?P<pk>[0-9]+)/$', views.ArtistDetail.as_view(), name='api-artist-detail'), 
+                       url(r'^api/artist/(?P<pk>[0-9]+)/$', views.ArtistDetail.as_view(), name='api-artist-detail'),
                        )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
