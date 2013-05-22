71e7ecff9df3efb71febd3c55153caae274aab27
Olivier Aubert <contact@olivieraubert.net>
2013-05-22 17:22:48 +0200

    Add Work/Artist list and detail views

--- kadist/urls.py
+++ kadist/urls.py
@@ -1,13 +1,22 @@
 from django.conf.urls import patterns, url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, simple, ListView, DetailView
 from django.contrib import admin
 from kadist import views
+from .models import Work, Artist
 
 urlpatterns = patterns('',
-                       url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'root.html'}),
+                       url(r'^$', simple.direct_to_template, {'template': 'root.html'}),
                        url(r'^tag/$', views.taglist, name='tag-list'),
                        url(r'^tag/(?P<kw>.+)$', views.tag, name='tag-detail'),
+                       url(r'^work/$', ListView.as_view(model=Work), name='work-list'),
+                       url(r'^work/(?P<pk>[0-9]+)/$', DetailView.as_view(model=Work, context_object_name='work'),
+                           name='work-detail'),
+                       url(r'^artist/$', ListView.as_view(model=Artist), name='artist-list'),
+                       url(r'^artist/(?P<pk>.+)/$', DetailView.as_view(model=Artist, context_object_name='artist'),
+                           name='artist-detail'),
+
+                       # REST API
                        url(r'^api/work/$', views.WorkList.as_view(), name='api-work-list'),
                        url(r'^api/work/(?P<pk>[0-9]+)/$', views.WorkDetail.as_view(), name='api-work-detail'),
                        url(r'^api/artist/$', views.ArtistList.as_view(), name='api-artist-list'),
