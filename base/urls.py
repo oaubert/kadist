d3b29182c1a7a16923c23ee8cb347481bf3d513f
Olivier Aubert <contact@olivieraubert.net>
2013-05-22 09:31:20 +0200

    Add login/logout urls

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': settings.STATIC_URL + 'img/favicon.ico'}),
+                       url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+                       url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
                        url(r'^$', RedirectView.as_view(url='/kadist/')),
                        url(r'^kadist/', include('kadist.urls')),
                        url(r'^admin/', include(admin.site.urls)),
