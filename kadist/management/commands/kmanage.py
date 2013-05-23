59455f3c894ee153f3f161814cf7a0811391805d
Olivier Aubert <contact@olivieraubert.net>
2013-05-23 11:26:02 +0200

    Implement xls import + tag extraction from description

--- kadist/management/commands/kmanage.py
+++ kadist/management/commands/kmanage.py
@@ -6,6 +6,7 @@ import re
 from django.core.management.base import BaseCommand, CommandError
 
 from kadist.models import Artist, Work
+from rake import RakeKeywordExtractor
 
 class Command(BaseCommand):
     args = '[command] [param]'
@@ -61,6 +62,56 @@ class Command(BaseCommand):
 
                 data[field] = value
             
+    def _import_works_from_xls(self, filename):
+        """Import from a xls file.
+        """
+        self.stdout.write("** Importing catalogue\n")
+        rake = RakeKeywordExtractor()
+
+        import xlrd
+        book = xlrd.open_workbook(filename)
+        s = book.sheet_by_index(0)
+        header = s.row_values(0)
+        
+        FIRSTNAME = 'Main Artist First Name'
+        SURNAME = 'Main Artist Surname'
+	TITLE = 'Main Title'
+	DATE = 'Main Date'
+	COUNTRY = 'Artist Country of Birth'
+	DESCRIPTION = 'About the artwork'
+
+        for n in range(1, s.nrows - 1):
+            self.stdout.write("[%d]" % n)
+            self.stdout.flush()
+            row = s.row_values(n)
+            data = dict(zip(header, row))
+
+            year = re.findall('(\d+)', data[DATE])
+            if year:
+                year = year[0]
+            else:
+                self.stdout.write("YEAR ? %s\n" % data[DATE])
+                year = 0
+            w = Work(title=data[TITLE].strip(),
+                     year=year,
+                     description=data[DESCRIPTION])
+            # Only take the first 2 words
+            cname = data[FIRSTNAME] + " " + data[SURNAME]
+            try:
+                creator = Artist.objects.get(name=cname)
+            except Artist.DoesNotExist:
+                # Have to create one
+                creator = Artist(name=cname, country=data[COUNTRY])
+                creator.save()
+
+            w.creator = creator
+            w.save()
+
+            # Extract tags from descriptions
+            tags = [ k for k in rake.extract(data[DESCRIPTION], incl_scores=False) if re.match('^\w+$', k) ]
+            self.stdout.write(("Tags: %s\n" % ", ".join(tags)).encode('utf-8'))
+            w.tags.add(*tags)
+
     def handle(self, *args, **options):
         if not args:
             self.print_help(sys.argv[0], sys.argv[1])
@@ -69,6 +120,7 @@ class Command(BaseCommand):
         args = args[1:]
         dispatcher = {
             'import': self._import_works_from_txt,
+            'xls': self._import_works_from_xls,
             }
         m = dispatcher.get(command)
         if m is not None:
