98ff3b93b02dfb4ad69c8cd15336ef5f4fde6989
Olivier Aubert <contact@olivieraubert.net>
2013-06-06 22:10:13 +0200

    Use prefixes for url patterns

--- kadist/urls.py
+++ kadist/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, RedirectView
 from kadist import views
 from .models import Work, Artist
 
@@ -9,9 +9,18 @@ urlpatterns = patterns('',
                        url(r'^$', login_required(TemplateView.as_view(template_name='root.html')), name='root'),
                        url(r'^tag/$', views.taglist_as_html, name='tag-list'),
                        url(r'^tag/(?P<kw>.+)$', views.tag, name='tag-detail'),
-                       url(r'^graph/(?P<type>tag)/(?P<kw>.*)$', login_required(TemplateView.as_view(template_name='graph.html')), name='graph'),
-                       url(r'^graph/(?P<type>work)/(?P<kw>.*)$', login_required(TemplateView.as_view(template_name='graph.html')), name='graph'),
-                       url(r'^graph/(?P<type>artist)/(?P<kw>.*)$', login_required(TemplateView.as_view(template_name='graph.html')), name='graph'),
+                       url(r'^graph/', include(patterns('',
+                                                        url(r'^$', RedirectView.as_view(url="tag/")),
+                                                        url(r'^(?P<type>tag)/(?P<kw>.*)$',
+                                                            login_required(TemplateView.as_view(template_name='graph.html')),
+                                                            name='tag-graph'),
+                                                        url(r'^(?P<type>work)/(?P<kw>.*)$',
+                                                            login_required(TemplateView.as_view(template_name='graph.html')),
+                                                            name='work-graph'),
+                                                        url(r'^(?P<type>artist)/(?P<kw>.*)$',
+                                                            login_required(TemplateView.as_view(template_name='graph.html')),
+                                                            name='artist-graph'),
+                                                        ))),
                        url(r'^work/$', login_required(ListView.as_view(model=Work)), name='work-list'),
                        url(r'^work/(?P<pk>[0-9]+)/$', login_required(DetailView.as_view(model=Work, context_object_name='work')),
                            name='work-detail'),
@@ -20,12 +29,26 @@ urlpatterns = patterns('',
                            name='artist-detail'),
 
                        # REST API
-                       url(r'^api/work/$', views.WorkList.as_view(), name='api-work-list'),
-                       url(r'^api/work/(?P<pk>[0-9]+)/$', views.WorkDetail.as_view(), name='api-work-detail'),
-                       url(r'^api/artist/$', views.ArtistList.as_view(), name='api-artist-list'),
-                       url(r'^api/artist/(?P<pk>[0-9]+)/$', views.ArtistDetail.as_view(), name='api-artist-detail'),
-                       url(r'^api/tag/$', views.taglist_as_json, name='api-tag-list'),
-                       url(r'^api/tag/(?P<kw>.+)$', views.tag_as_json, name='api-tag-detail'),
+                       url(r'^api/', include(patterns('',
+                                                      url(r'^work/$', 
+                                                          views.WorkList.as_view(), 
+                                                          name='api-work-list'),
+                                                      url(r'^work/(?P<pk>[0-9]+)/$', 
+                                                          views.WorkDetail.as_view(), 
+                                                          name='api-work-detail'),
+                                                      url(r'^artist/$', 
+                                                          views.ArtistList.as_view(), 
+                                                          name='api-artist-list'),
+                                                      url(r'^artist/(?P<pk>[0-9]+)/$', 
+                                                          views.ArtistDetail.as_view(), 
+                                                          name='api-artist-detail'),
+                                                      url(r'^tag/$', 
+                                                          views.taglist_as_json, 
+                                                          name='api-tag-list'),
+                                                      url(r'^tag/(?P<kw>.+)$', 
+                                                          views.tag_as_json, 
+                                                          name='api-tag-detail'),
+                                                      )))
                        )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
