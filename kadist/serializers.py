37e5358fdf7ddd05da90113a464cf698149d72a2
Olivier Aubert <contact@olivieraubert.net>
2013-08-12 23:49:55 +0200

    Serialize kurl (Kadist url) field

--- kadist/serializers.py
+++ kadist/serializers.py
@@ -4,30 +4,34 @@ from .models import Work, Artist
 class ArtistReferenceSerializer(serializers.HyperlinkedModelSerializer):
     """Minimal serialization for an artist reference.
     """
+    kurl = serializers.CharField(source="url")
     class Meta:
         model = Artist
-        fields = ('url', 'id', 'name', )
+        fields = ('url', 'id', 'name', 'kurl')
 
 class WorkReferenceSerializer(serializers.HyperlinkedModelSerializer):
     """Minimal serialization for a work reference.
     """
     artist = ArtistReferenceSerializer(source='creator', read_only=True)
+    kurl = serializers.CharField(source="url")
     class Meta:
         model = Work
-        fields = ('url', 'id', 'artist', 'title')
+        fields = ('url', 'id', 'artist', 'title', 'kurl')
 
 class WorkSerializer(serializers.HyperlinkedModelSerializer):
     tags = serializers.RelatedField(source='weighted_tags')
     similar = WorkReferenceSerializer(source='tags.similar_objects')
     artist = ArtistReferenceSerializer(source='creator')
+    kurl = serializers.CharField(source="url")
     class Meta:
         model = Work
-        fields = ('url', 'id', 'artist', 'title', 'worktype', 'technique','dimensions', 'description', 'tags', 'similar')
+        fields = ('url', 'id', 'artist', 'title', 'worktype', 'technique','dimensions', 'description', 'tags', 'similar', 'kurl')
 
 class ArtistSerializer(serializers.HyperlinkedModelSerializer):
     works = WorkReferenceSerializer(source='works')
     tags = serializers.RelatedField(source='weighted_tags')
+    kurl = serializers.CharField(source="url")
 
     class Meta:
         model = Artist
-        fields = ('url', 'id', 'name', 'country', 'description', 'works', 'tags')
+        fields = ('url', 'id', 'name', 'country', 'description', 'works', 'tags', 'kurl')
