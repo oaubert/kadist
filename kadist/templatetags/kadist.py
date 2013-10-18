b36c7ab12cba6d1a97efe80f109bc2b9401b9ddd
Olivier Aubert <contact@olivieraubert.net>
2013-10-18 15:53:57 +0200

    Implement callMethod/args templatetags

--- kadist/templatetags/kadist.py
+++ kadist/templatetags/kadist.py
@@ -19,3 +19,19 @@ def tagsize(c):
     else:
         return long(TAG_MINSIZE + TAG_FACTOR * (c - TAG_MINCOUNT))
 
+def callMethod(obj, methodName):
+    method = getattr(obj, methodName)
+    if obj.__dict__.has_key("__callArg"):
+        ret = method(*obj.__callArg)
+        del obj.__callArg
+        return ret
+    return method()
+
+def args(obj, arg):
+    if not obj.__dict__.has_key("__callArg"):
+        obj.__callArg = []
+    obj.__callArg += [arg]
+    return obj
+
+register.filter("call", callMethod)
+register.filter("args", args)
